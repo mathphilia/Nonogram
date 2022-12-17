341225d74c7476bc042ce274f2af01f81a4b5e7e
mathphilia <104004553+mathphilia@users.noreply.github.com>
2022-12-17 21:20:09 +0900

    update

--- nonogram.js
+++ nonogram.js
@@ -1,42 +1,95 @@
 onload = () => {
-    let pushed = false, color = 1, data;
+    let canRedo = false, check = 0, color = 'black', data = [], log = [], logIndex = -1, pushed = false;
     let ascii = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     function change(elem){
-        switch(color){
+        switch(check){
             case 0:
+                elem.style.background = color;
+                elem.style.color = '';
                 elem.innerText = '';
                 break;
             case 1:
-                elem.style.background = 'black';
+                elem.style.background = '';
+                elem.style.color = color;
+                elem.innerText = '×';
                 break;
             case 2:
+                elem.innerText = '';
                 elem.style.background = '';
-                elem.innerText = '×';
+                elem.style.color = '';
                 break;
         }
     }
 
-    function convert(text, mode){
-        if(mode == 1){
-            let n = 0n;
-            for(let c of text){
-                if(!ascii.includes(c))return NaN;
-                n = n * 62n + BigInt(ascii.indexOf(c));
-            }
-            return n.toString(3);
-        }else if(mode == 2){
-            let n = 0n;
-            for(let c of text){
-                n = n * 3n + BigInt(c);
-            }
-            let ret = '';
-            while(n){
-                ret = ascii[n % 62n] + ret;
-                n /= 62n;
+    function convert1(text, size){
+        if(text.length == 1){
+            let ret = [];
+            for(let i = 0;i < size;i++){
+                ret.push(parseInt(text));
             }
             return ret;
         }
+        let tableString = text.slice(-3);
+        text = text.slice(0, -3);
+        let tableNum = 0;
+        for(let c of tableString){
+            if(!ascii.includes(c)){
+                return NaN;
+            }
+            tableNum = tableNum * 62 + ascii.indexOf(c);
+        }
+        let table = [];
+        for(let c of tableNum.toString(2).padStart(15, 0)){
+            table.push(Number(c) ? Math.max(0, ...table) + 1 : 0);
+        }
+        let tableLength = Math.max(...table);
+        let n = 0n;
+        for(let c of text){
+            if(!ascii.includes(c)){
+                return NaN;
+            }
+            n = n * 62n + BigInt(ascii.indexOf(c));
+        }
+        let ret = [];
+        for(let c of n.toString(tableLength).padStart(size, 0)){
+            ret.push(table.indexOf(parseInt(c, tableLength) + 1));
+        }
+        return ret;
+    }
+
+    function convert2(arr){
+        let used = [...Array(15).keys()].map(a => Number(arr.includes(a)));
+        let table = [];
+        for(let i = 0;i < 15;i++){
+            table.push(used[i] ? Math.max(0, ...table) + 1 : 0);
+        }
+        let tableLength = Math.max(...table);
+        if(tableLength == 1){
+            let only = used.indexOf(1);
+            if(only < 14){
+                return only.toString(16);
+            }else{
+                return '';
+            }
+        }
+        let fixed = arr.map(a => (table[a] - 1).toString(15)).join('');
+        let n = 0n;
+        for(let c of fixed){
+            n *= BigInt(tableLength);
+            n += BigInt(parseInt(c, 16));
+        }
+        let ret = '';
+        let usedNum = parseInt(used.join(''), 2);
+        for(let i = 0;i < 3;i++){
+            ret = ascii[usedNum % 62] + ret;
+            usedNum = (usedNum - usedNum % 62) / 62;
+        }
+        while(n){
+            ret = ascii[n % 62n] + ret;
+            n /= 62n;
+        }
+        return ret;
     }
 
     function genCells(rowInfo, colInfo){
@@ -44,18 +97,36 @@ onload = () => {
         let row = '<tr>' + '<td></td>'.repeat(colInfo.length + 1) + '</tr>';
         tbodyElem.innerHTML = row.repeat(rowInfo.length + 1);
         let tdElems = document.getElementsByTagName('td');
-        let width = 7;
-        for(let i = 1; i <= rowInfo.length; i++){
+        let width = 0;
+        for(let i = 1;i <= rowInfo.length;i++){
             let text = rowInfo[i - 1].join(' ');
             tdElems[i * (colInfo.length + 1)].innerHTML = text;
             width = Math.max(width, text.length);
         }
-        for(let i = 1; i <= colInfo.length; i++){
+        for(let i = 1;i <= colInfo.length;i++){
             tdElems[i].innerText = colInfo[i - 1].join('\n');
         }
-        let leftmost = document.getElementById('leftmost');
-        leftmost.innerText = `td:nth-child(${colInfo.length + 1}n+1){text-align:right;width:${width * 9}px}`;
+        tdElems[0].style.width = width * 10 + 'px';
+        tdElems[0].style.textAlign = 'center';
         setMethod();
+    }
+
+    function loadLog(idx){
+        let codeArea = document.getElementById('code');
+        let tmp = codeArea.value;
+        codeArea.value = log[idx];
+        document.getElementById('1').click();
+        codeArea.value = tmp;
+    }
+
+    function saveLog(){
+        let codeArea = document.getElementById('code');
+        let tmp = codeArea.value;
+        document.getElementById('2').click();
+        if(log[logIndex] != codeArea.value){
+            log[++logIndex] = codeArea.value;
+        }
+        codeArea.value = tmp;
     }
 
     function setMethod(){
@@ -65,40 +136,105 @@ onload = () => {
         let rows = trElems.length;
         let cols = cells / rows;
         data = Array(cells);
-        for(let index = cols; index < cells; index++){
-            if(index % cols == 0)continue;
+        for(let index = cols;index < cells;index++){
+            if(index % cols == 0){
+                continue;
+            }
             elem = tdElems[index];
-            data[index] = 0;
+            data[index] = 2;
 
             elem.onmousedown = function(){
                 pushed = true;
-                data[index] = color = (data[index] + 1) % 3;
+                if(data[index] == 0 && this.style.background != color){
+                    check = data[index];
+                }else if(data[index] == 1 && this.style.color != color){
+                    check = data[index];
+                }else{
+                    data[index] = check = (data[index] + 1) % 3;
+                }
                 change(this);
             }
 
             elem.onmouseout = function(){
-                for(let i = index % cols; i < cells; i += cols){
-                    if(data[i] == 1)continue;
-                    tdElems[i].style.background = '';
+                for(let i = index % cols;i < cells;i += cols){
+                    if(data[i] != 0 && tdElems[i].style.background != 'deeppink'){
+                        tdElems[i].style.background = '';
+                    }
                 }
-                trElems[Math.floor(index / cols)].style.background = ''
+                trElems[Math.floor(index / cols)].style.background = '';
             }
 
             elem.onmouseover = function(){
                 if(pushed){
-                    data[index] = color;
+                    data[index] = check;
                     change(this);
                 }
-                for(let i = index % cols; i < cells; i += cols){
-                    if(data[i] == 1)continue;
-                    tdElems[i].style.background = '#DDD';
+                for(let i = index % cols;i < cells;i += cols){
+                    if(data[i] != 0 && tdElems[i].style.background != 'deeppink'){
+                        tdElems[i].style.background = '#DDD';
+                    }
                 }
-                trElems[Math.floor(index / cols)].style.background = '#DDD'
+                trElems[Math.floor(index / cols)].style.background = '#DDD';
+            }
+        }
+
+        let btn = document.createElement('button');
+        tdElems[0].appendChild(btn);
+        btn.innerText = 'check\nanswer';
+        btn.style.margin = '5px 5px';
+        btn.onclick = function(){
+            let valid = true;
+            for(let i = 1;i < rows;i++){
+                let elem = tdElems[i * cols], countArray = [], count = 0;
+                for(let j = 0;j < cols;j++){
+                    if(colorList.includes(tdElems[i * cols + j].style.background)){
+                        count++;
+                    }else if(count){
+                        countArray.push(count);
+                        count = 0;
+                    }
+                }
+                if(count || !countArray.length){
+                    countArray.push(count);
+                }
+                if(elem.innerText != countArray.join(' ')){
+                    elem.style.background = 'deeppink';
+                    valid = false;
+                }else{
+                    elem.style.background = '';
+                }
+            }
+            for(let j = 1;j < cols;j++){
+                let elem = tdElems[j], countArray = [], count = 0;
+                for(let i = 0;i < rows;i++){
+                    if(colorList.includes(tdElems[i * cols + j].style.background)){
+                        count++;
+                    }else if(count){
+                        countArray.push(count);
+                        count = 0;
+                    }
+                }
+                if(count){
+                    countArray.push(count);
+                }
+                if(elem.innerText != countArray.join('\n')){
+                    elem.style.background = 'deeppink';
+                    valid = false;
+                }else{
+                    elem.style.background = '';
+                }
+            }
+            if(valid){
+                alert('correct!!');
             }
         }
     }
 
     onmouseup = function(){
+        if(pushed){
+            saveLog();
+            canRedo = false;
+        }
         pushed = false;
     }
 
@@ -113,7 +249,7 @@ onload = () => {
         let rows = arr[0], cols = arr[1];
         let rowInfo = inputs[1].split('/').map(a => a.trim().split(' ').map(Number));
         let colInfo = inputs[2].split('/').map(a => a.trim().split(' ').map(Number));
-        let cellData = inputs[3] ? convert(inputs[3], 1) : '';
+        let cellData = inputs[3] ? convert1(inputs[3], rows * cols) : '';
         if((rowInfo + colInfo + cellData).includes('NaN')){
             document.getElementById('message').innerText = 'フォーマットエラー';
             return;
@@ -124,15 +260,26 @@ onload = () => {
             document.getElementById('message').innerText = '行数が行の情報数と一致しません';
         }else{
             genCells(rowInfo, colInfo);
-            cellData = cellData.padStart(rows * cols, 0);
-            for(let i = 1; i <= rows; i++){
-                for(let j = 1; j <= cols; j++){
-                    color = Number(cellData[(i - 1) * cols + (j - 1)]);
-                    data[i * (cols + 1) + j] = color;
-                    change(tdElems[i * (cols + 1) + j]);
+            document.getElementById('message').innerText = example;
+            if(cellData){
+                for(let i = 1;i <= rows;i++){
+                    for(let j = 1;j <= cols;j++){
+                        let checkAndColor = cellData[(i - 1) * cols + (j - 1)];
+                        color = colorList[checkAndColor % 7];
+                        check = (checkAndColor - checkAndColor % 7) / 7;
+                        data[i * (cols + 1) + j] = check;
+                        change(tdElems[i * (cols + 1) + j]);
+                    }
+                }
+                for(let button of document.getElementsByName('color-radio')){
+                    if(button.class == 'selected'){
+                        color = button.value;
+                        break;
+                    }
                 }
             }
         }
+        saveLog();
     }
 
     document.getElementById('2').onclick = function(){
@@ -142,33 +289,81 @@ onload = () => {
         let rows = trElems.length;
         let cols = cells / rows;
         let rowInfo = [], colInfo = [];
-        for(let i = 1; i < rows; i++){
+        for(let i = 1;i < rows;i++){
             let info = tdElems[i * cols].innerText;
             rowInfo.push(info.split(' ').map(Number));
         }
-        for(let i = 1; i < cols; i++){
-            let info = tdElems[i].innerText
+        for(let i = 1;i < cols;i++){
+            let info = tdElems[i].innerText;
             colInfo.push(info.split('\n').map(Number));
         }
-        let cellData = '';
-        for(let i = 1; i < rows; i++){
-            for(let j = 1; j < cols; j++){
+        let cellData = [], _color;
+        for(let i = 1;i < rows;i++){
+            for(let j = 1;j < cols;j++){
                 let elem = tdElems[i * cols + j];
-                if(elem.style.background == 'black'){
-                    cellData += 1;
-                }else if (elem.innerText == '×'){
-                    cellData += 2;
+                if(colorList.includes(elem.style.background)){
+                    _color = elem.style.background;
+                    cellData.push(colorList.indexOf(_color));
+                }else if(elem.innerText == '×'){
+                    _color = elem.style.color;
+                    cellData.push(colorList.indexOf(_color) + 7);
                 }else{
-                    cellData += 0;
+                    cellData.push(14);
                 }
             }
         }
         document.getElementById('code').value = `${rows - 1} ${cols - 1}\n`;
         document.getElementById('code').value += rowInfo.map(a => a.join(' ')).join('/') + '\n';
         document.getElementById('code').value += colInfo.map(a => a.join(' ')).join('/') + '\n';
-        document.getElementById('code').value += convert(cellData, 2);
+        document.getElementById('code').value += convert2(cellData);
+    }
+
+    document.getElementById('undo').onclick = function(){
+        if(0 < logIndex){
+            loadLog(--logIndex);
+        }
+        canRedo = true;
+    }
+
+    document.getElementById('redo').onclick = function(){
+        if(canRedo && log[logIndex + 1]){
+            loadLog(++logIndex);
+        }
+    }
+
+    onkeyup = function(event){
+        if(!event.ctrlKey){
+            return;
+        }
+        if(event.keyCode == 0x5a && event.shiftKey){
+            document.getElementById('redo').click();
+        }else if(event.keyCode == 0x5a){
+            document.getElementById('undo').click();
+        }else if(event.keyCode == 0x59){
+            document.getElementById('redo').click();
+        }
+    }
+
+    let buttons = document.getElementsByName('color-radio');
+    let colorList = []
+    for(let button of buttons){
+        button.style.backgroundColor = button.value;
+        button.onclick = function(){
+            color = this.value;
+            for(let _button of buttons){
+                if(_button == this){
+                    _button.className = 'selected';
+                    _button.style.outlineColor = color;
+                }else{
+                    _button.className = '';
+                }
+            }
+        }
+        colorList.push(button.value);
     }
 
     document.getElementById('code').placeholder = '行数 列数\n各行の情報(横書きの数)\n各列の情報(縦書きの数)\n各セルの情報(必要に応じて)';
     genCells([[5], [1, 1, 1], [5], [1, 1, 1], [5]], [[5], [1, 1, 1], [5], [1, 1, 1], [5]]);
+    let example = document.getElementById('message').innerText = '<sample>\n10 10\n3/1/1 5/5/0/5/1 1/1 1/1 1/5\n1 5/1 1 1 1/1 1 1 1/4 1 1/1 5/1/1/1/1/1';
+    saveLog();
 };
